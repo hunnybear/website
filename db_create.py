db2d7f0dcd8e00ac279aaeea721cb68b97969094
hunnybear <me@tylerjachetta.net>
2016-08-21 00:44:06 -0500

    guess I never linted db_create.py. missed namespace path on a thingy.

--- db_create.py
+++ db_create.py
@@ -5,8 +5,16 @@ import app
 import os.path
 
 app.db.create_all()
-if not os.path.exists( app.config.SQLALCHEMY_MIGRATE_REPO ):
-    api.create( app.config.SQLALCHEMY_MIGRATE_REPO, 'database repository' )
-    api.version_control( app.config.SQLALCHEMY_DATABASE_URI, app.config.SQLALCHEMY_MIGRATE_REPO)
+if not os.path.exists(app.config.SQLALCHEMY_MIGRATE_REPO):
+    api.create(app.config.SQLALCHEMY_MIGRATE_REPO, 'database repository')
+    api.version_control(
+        app.config.SQLALCHEMY_DATABASE_URI,
+        app.config.SQLALCHEMY_MIGRATE_REPO
+    )
+
 else:
-    api.version_control( app.config.SQLALCHEMY_DATABASE_URI, app.config.SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+    api.version_control(
+        app.config.SQLALCHEMY_DATABASE_URI,
+        app.config.SQLALCHEMY_MIGRATE_REPO,
+        api.version(app.config.SQLALCHEMY_MIGRATE_REPO)
+    )
